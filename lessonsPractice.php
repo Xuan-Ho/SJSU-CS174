5180770187a6a6b76f50b1e2bfc4edde67c43dfb
teoorxuan <teoorxuan@gmail.com>
2017-11-02 00:12:48 -0700

    new lesson

---
+++ lessonsPractice.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'login.php';
+$conn = new mysqli($hn, $un, $pw, $db);
+$if($conn->connect_error)
+    die($conn->connect_error);
+
+function my_sql_fatal_error($msg)
+{
+    $msg2 = mysql_error();
+    echo <<< _END
+We are sorry, but it was not possible to complete
+the requested task. The error message we got was:
+
+    <p>$msg: $msg2</p>
+
+Please click the back button on your browser
+and try again. If you are still having problems,
+please <a href="mailto:admin@server.com">email
+our administrator</a>. Thank you.
+_END;
+}
+>
